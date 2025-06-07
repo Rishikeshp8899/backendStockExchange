6eb9a2eddcb41dd6da33d5122e7d55b1e6edf379
Rishikeshp8899 <patilrishikeshm88@gmail.com>
2025-06-07 22:51:10 +0530

    "Add Share Curd Service"

---
+++ Share-Crud-Service/src/main/java/com/richim/share_crud_service/Share_Crud_Service/ShareCrudServiceApplication.java
@@ -0,0 +1,13 @@
+package com.richim.share_crud_service.Share_Crud_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareCrudServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareCrudServiceApplication.class, args);
+	}
+
+}
---
+++ Share-Crud-Service/src/test/java/com/richim/share_crud_service/Share_Crud_Service/ShareCrudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richim.share_crud_service.Share_Crud_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareCrudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
